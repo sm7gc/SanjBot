d31e662caea620456c23d1e7e636e3a78ed3746e
Sanjana Mendu <sm7gc@virginia.edu>
2019-01-04 10:41:01 -0500

    added input option dialogue

--- convo.js
+++ convo.js
@@ -9,10 +9,33 @@ function init() {
 
     botui.message.bot({
         delay: 700,
-        content: 'Welcome to SanjBot. The current survey protcol is modeled after the PHQ-9'
+        content: 'Welcome to SanjBot. The current survey protocol is modeled after the PHQ-9'
     }).then(function () {
-        survey(protocol, 0);
+        botui.message.bot({
+            delay: 500,
+            content: 'Would you prefer to type your answer or select from multiple choice?'
+        }).then(function () {
+            botui.action.button({
+                delay: 1000,
+                action: [{
+                    text: "Type",
+                    value: "text"
+                }, {
+                    text: "Select",
+                    value: "button"
+                }]
+            }).then(function (res) {
+                if (res.value == 'text') {
+                    surveyText(protocol, 0);
+                } else if (res.value == 'button') {
+                    survey(protocol, 0);
+                }
+            });
+        })
     });
+    //        .then(function () {
+    //        surveyText(protocol, 0);
+    //    });
 }
 
 function parseObj(obj) {
@@ -50,28 +73,34 @@ function survey(protocol, i) {
                 action: curr.options
             })
         })
-        //        .then(function (res) {
-        //            return botui.message.bot({
-        //                delay: 1500,
-        //                loading: true, // pretend like we are doing something
-        //                content: 'You selected: ' + res.value
-        //            });
-        //        })
         .then(function () {
             survey(protocol, i + 1);
         });
 }
 
-//function getProtocol(filepath) {
-//    $.getJSON("protocol.json", function (data) {
-//        var obj = JSON.parse(data);
-//        var obj_str = JSON.stringify(obj, null, 2);
-//
-//        console.log(obj_str);
-//
-//    });
-//}
+function surveyText(protocol, i) {
+    if (i >= Object.keys(protocol).length) {
+        done();
+        return;
+    }
 
+    var curr = parseObj(protocol[i]);
+
+    botui.message.bot({
+            delay: 500,
+            content: curr.question
+        })
+        .then(function () {
+            return botui.action.text({ // show 'text' action
+                action: {
+                    placeholder: ''
+                }
+            });
+        })
+        .then(function () {
+            surveyText(protocol, i + 1);
+        });
+}
 
 function done() {
 
